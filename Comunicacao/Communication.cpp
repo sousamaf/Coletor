50c75ef455886f32d09cb50d96a3fe28a8dca28e
EldonLucena <eldon.lucena@gmail.com>
2013-04-06 22:42:54 -0300

    Update Communication.cpp

--- Comunicacao/Communication.cpp
+++ Comunicacao/Communication.cpp
@@ -6,9 +6,13 @@
  */
 
 #include "Communication.h"
+#include <sstream.h>
+#include <iomanip.h>
 
 namespace middleware
 {
+	string sDevice;
+	
 
 	Communication::Communication()
 	{
@@ -22,29 +26,72 @@ namespace middleware
 	}
 
 	/*
-	 * O que isso faz?
+	 * metodo de iniicalização do módulo de comunicação
 	 */
-	int Communication::init(int device, int frequence)
+	int Communication::init(String device, int frequence)
 	{
+		sDevice = device;
 
+		switch (device)
+		{
+			case 'XBEE'
+			Serial.begin(frequence);
+			
+			break;
+			
+			case 'APC220'
+			Serial.begin(frequence);
+			
+			break;
+		}
 		return 0;
 	}
 
 	/*
-	 * O que isso faz?
+	 * Recebe como parametro uma string, converte para tipo de dados do módulo
+	 * e enviao sinal
 	 */
 	int Communication::send(String message)
-	{
-
+	{		
+		
+		std::string str = message;
+		std::vector<char> bytes(str.begin(), str.end());
+		
+		switch (sDevice)
+		{
+			case 'XBEE'
+			Serial.print(bytes);			   
+			break;
+			
+			case 'APC220'
+			Serial.print(bytes);			
+			break;
+		}
 		return 0;
 	}
 
 	/*
-	 * O que isso faz
+	 * Recebe como parametro uma string, converte para tipo de dados de leitura
+	 * e retorna o valor
 	 */
-	String Communication::read(String message)
+	String Communication::read(byte[] message)
 	{
-
+			
+		std::vector<byte>  source(str.begin(),str.end());
+		encrypt(&source[0],source.size());
+		
+		switch (sDevice)
+		{
+			case 'XBEE'
+			Serial.print("");			   
+			break;
+			
+			case 'APC220'
+			Serial.print("");			
+			break;
+		}
+		return 0;
+		
 		return "";
 	}
 
